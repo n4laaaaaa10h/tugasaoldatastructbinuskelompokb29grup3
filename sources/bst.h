5eca7ff197186c116245870fefc2a0fe42c47bc7
n4laaaaaa10h <pnaladhipa@gmail.com>
2026-06-12 18:08:36 +0700

    added bst and queue cred: koivizo (keisha)

---
+++ sources/bst.h
@@ -0,0 +1,173 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//bst node structure
+typedef struct Node{
+	int id;                //Menu item id
+	char name[50];         //Item name
+	char category[20];     //category 
+	float price;           //item price
+	struct Node *left;
+	struct Node *right;
+} Node;
+
+//Create new Node
+Node* createNode(int id, char name[], char category[], float price) {
+    Node *newNode = (Node*)malloc(sizeof(Node));
+    newNode->id = id;
+    
+    strcpy(newNode->category, category);
+    
+    newNode->price = price;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+//insert into BST
+Node* insert(Node *root, int id, char name[], char category[], float price) {
+    if (root == NULL)
+        return createNode(id, name, category, price);
+
+    if (id < root->id)
+        root->left = insert(root->left, id, name, category, price);
+    else if (id > root->id)
+        root->right = insert(root->right, id, name, category, price);
+
+    return root;
+}
+
+//Search item
+Node* search(Node *root, int id) {
+    if (root == NULL || root->id == id)
+        return root;
+
+    if (id < root->id)
+        return search(root->left, id);
+    else
+        return search(root->right, id);
+}
+
+//display menu
+void displayMenu(Node *root) {
+    if (root != NULL) {
+        displayMenu(root->left);
+        printf("ID: %d | Name: %s | Category: %s | Price: %.2f\n",
+               root->id, root->name, root->category, root->price);
+        displayMenu(root->right);
+    }
+}
+
+//find min value
+Node* findMin(Node *root) {
+    if (root == NULL)
+        return NULL;
+
+    while (root->left != NULL)
+        root = root->left;
+
+    return root;
+}
+
+//delete item
+Node* deleteItem(Node *root, int id) {
+	if (root == NULL)
+    return root;
+	
+	if (id < root->id) {
+        root->left = deleteItem(root->left, id);
+    }
+    else if (id > root->id) {
+        root->right = deleteItem(root->right, id);
+    }
+	
+	else{
+		if (root->left == NULL) {
+			Node *temp = root->right;
+			free(root);
+			return temp;
+		}else if (root->right == NULL) {
+			Node *temp = root->left;
+			free(root);
+			return temp;
+		}
+		
+		//two children
+		Node *temp = findMin(root->right);
+
+        root->id = temp->id;
+        strcpy(root->name, temp->name);
+        strcpy(root->category, temp->category);
+        root->price = temp->price;
+
+        root->right = deleteItem(root->right, temp->id);
+    }
+
+    return root;
+}
+
+int main() {
+    Node *root = NULL;
+    int choice, id;
+    char name[50];
+    char category[20];
+    float price;
+
+    do {
+        printf("\nRESTAURANT CASHIER\n");
+        printf("1. Add Menu Item\n");
+        printf("2. Search Menu Item\n");
+        printf("3. Display Menu\n");
+        printf("4. Delete Menu Item\n");
+        printf("5. Exit\n");
+        printf("Choose: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter Item Name: ");
+				scanf(" %[^\n]", name);
+				
+				printf("Enter Category: ");
+				scanf(" %[^\n]", category);
+				
+				printf("Enter Price: ");
+				scanf("%f", &price);
+				
+				root = insert(root, id, name, category, price);
+                printf("Item added successfully!\n");
+                break;
+
+            case 2:
+                printf("Enter Item ID to search: ");
+                scanf("%d", &id);
+                Node *found = search(root, id);
+                if (found)
+                    printf("Found: %s | Price: %.2f\n", found->name, found->price);
+                else
+                    printf("Item not found.\n");
+                break;
+
+            case 3:
+                printf("\n--- MENU LIST ---\n");
+                displayMenu(root);
+                break;
+
+            case 4:
+                printf("Enter Item ID to delete: ");
+                scanf("%d", &id);
+                root = deleteItem(root, id);
+                printf("Item deleted if it existed.\n");
+                break;
+
+            case 5:
+                printf("Thank you! Exiting...\n");
+                break;
+
+            default:
+                printf("Invalid choice.\n");
+        }
+    } while (choice != 5);
+
+    return 0;
+}
---
+++ sources/queue.h
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int orderID;
+    char menuName[50];
+    int quantity;
+    char status[20];
+    struct Order *next;
+} Order;
+
+//front and rear
+Order *front = NULL;
+Order *rear = NULL;
+
+//create new order
+Order* createOrder(int orderID, char menuName[], int quantity) {
+    Order *newOrder = (Order*)malloc(sizeof(Order));
+    newOrder->orderID = orderID;
+    strcpy(newOrder->menuName, menuName);
+    newOrder->quantity = quantity;
+    strcpy(newOrder->status, "Waiting");
+    newOrder->next = NULL;
+    return newOrder;
+}
+
+//enqueue order (insert)
+void enqueueOrder(int orderID, char menuName[], int quantity) {
+    Order *newOrder = createOrder(orderID, menuName, quantity);
+
+    if (rear == NULL) {
+        front = rear = newOrder;
+        return;
+    }
+
+    rear->next = newOrder;
+    rear = newOrder;
+}
+
+//dequeue order (delete)
+void dequeueOrder() {
+    if (front == NULL) {
+        printf("No orders in kitchen queue.\n");
+        return;
+    }
+
+    Order *temp = front;
+    printf("Order Completed:\n");
+    printf("ID: %d | Menu: %s | Qty: %d\n",
+           temp->orderID, temp->menuName, temp->quantity);
+
+    front = front->next;
+    free(temp);
+
+    if (front == NULL)
+        rear = NULL;
+}
+
+//display queue
+void displayKitchenQueue() {
+    if (front == NULL) {
+        printf("Kitchen queue is empty.\n");
+        return;
+    }
+
+    Order *temp = front;
+    printf("\n--- ANTRIAN PESANAN DAPUR ---\n");
+    while (temp != NULL) {
+        printf("ID: %d | Menu: %s | Qty: %d | Status: %s\n",
+               temp->orderID,
+               temp->menuName,
+               temp->quantity,
+               temp->status);
+        temp = temp->next;
+    }
+}
+
+//status update
+void startCooking() {
+    if (front == NULL) {
+        printf("No order to cook.\n");
+        return;
+    }
+
+    strcpy(front->status, "Cooking");
+    printf("Now Cooking:\n");
+    printf("ID: %d | Menu: %s\n",
+           front->orderID, front->menuName);
+}
+
+//main
+int main() {
+    int choice, orderID, quantity;
+    char menuName[50];
+
+    do {
+        printf("\nSISTEM ANTRIAN DAPUR\n");
+        printf("1. Tambah Pesanan \n");
+        printf("2. Mulai Masak Pesanan Terdepan\n");
+        printf("3. Pesanan Selesai (Dequeue)\n");
+        printf("4. Lihat Antrian Dapur\n");
+        printf("5. Keluar\n");
+        printf("Pilih: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Order ID: ");
+                scanf("%d", &orderID);
+                printf("Nama Menu: ");
+                scanf(" %[^\n]", menuName);
+                printf("Jumlah: ");
+                scanf("%d", &quantity);
+                enqueueOrder(orderID, menuName, quantity);
+                printf("Pesanan masuk antrian dapur.\n");
+                break;
+
+            case 2:
+                startCooking();
+                break;
+
+            case 3:
+                dequeueOrder();
+                break;
+
+            case 4:
+                displayKitchenQueue();
+                break;
+
+            case 5:
+                printf("Sistem dapur ditutup.\n");
+                break;
+
+            default:
+                printf("Pilihan tidak valid.\n");
+        }
+    } while (choice != 5);
+
+    return 0;
+}
